3815e82c84bb35466cde3c995a06bb43983e20a4
Andreas Profous <ap@profous.de>
2022-12-07 23:00:41 +0100

    day6 part1

---
+++ day6/message.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from toolz import sliding_window
+
+
+def read_input() -> str:
+    with open("input.txt", "r") as file:
+        lines = [line for line in file]
+    return lines[0]
+
+
+def first_marker(line: str) -> int:
+    fours = list(sliding_window(4, line))
+    for i, four in enumerate(fours):
+        if len(set(four)) == 4:
+            return i + 4
+
+
+def main():
+    line = read_input()
+    print(first_marker(line))
+
+
+if __name__ == "__main__":
+    main()
